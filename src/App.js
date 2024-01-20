4a0322bcb5b2f76b2f5a47e25d4ff5f42fbf47de
Vijayaditya <vijayaditya.eng@gmail.com>
2024-01-20 11:02:35 +0530

    Removed Animation

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Terminal from './Terminal';
-import Animation from './Animation';
+// import Animation from './Animation';
 
 function App() {
-  const [showAnimation, setShowAnimation] = useState(true);
+  // const [showAnimation, setShowAnimation] = useState(true);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setShowAnimation(window.innerWidth >= 1200);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setShowAnimation(window.innerWidth >= 1200);
+  //   };
 
-    // Set the initial state based on the current width
-    handleResize();
+  //   // Set the initial state based on the current width
+  //   handleResize();
 
-    // Add event listener for window resize
-    window.addEventListener('resize', handleResize);
+  //   // Add event listener for window resize
+  //   window.addEventListener('resize', handleResize);
 
-    // Remove event listener on component unmount
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  //   // Remove event listener on component unmount
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, []);
 
   return (
     <>
