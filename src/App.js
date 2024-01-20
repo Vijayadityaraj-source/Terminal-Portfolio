80d37036b073ef59c39722b8feea1baf2947b342
Vijayaditya <vijayaditya.eng@gmail.com>
2024-01-20 11:03:51 +0530

    Removed Animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Terminal from './Terminal';
